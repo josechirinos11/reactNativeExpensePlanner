8963efcad83352cd71bf5e33db46df3aec021ccd
josechirinos11 <josechirinos11@gmail.com>
2024-01-29 21:35:31 -0400

    carga de proyecto

---
+++ App.tsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import Headers from './src/components/Headers';
+
+import {
+  SafeAreaView,
+  
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+
+const App = () => {
+
+  return (
+    <View>
+    <Headers />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
