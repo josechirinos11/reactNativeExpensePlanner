169e71216a327ab12ba2ba6f9bb9ce509aee989e
josechirinos11 <josechirinos11@gmail.com>
2024-02-12 13:44:49 -0400

    12 enero 2024/control de presupuesto modal

--- App.tsx
+++ App.tsx
@@ -1,28 +1,123 @@
-
-import React from 'react';
-import Headers from './src/components/Headers';
-
+import React, { useState, useEffect } from 'react';
 import {
-  SafeAreaView,
-  
+  ScrollView,
   StyleSheet,
-  Text,
   View,
+  Alert,
+  Pressable,
+  Image,
+  Modal
 } from 'react-native';
-
-
-
+//import AsyncStorage from '@react-native-async-storage/async-storage'
+import Header from './src/components/Headers';
+import NuevoPresupuesto from './src/components/NuevoPresupuesto';
+import ControlPresupuesto from './src/components/ControlPresupuesto';
+import FormularioGasto from './src/components/FormularioGasto';
+//import ListadoGastos from './src/components/ListadoGastos';
+//import Filtro from './src/components/Filtro';
+//import { generarId } from './src/helpers'
 
 const App = () => {
+  const [ isValidPresupuesto, setIsValidPresupuesto ] = useState(false)
+  const [ presupuesto, setPresupuesto ] = useState(0)
+  const [ gastos, setGastos ] = useState([])
+  const [ modal, setModal] = useState(false)
+  const [ gasto, setGasto ] = useState({})
+  const [ filtro, setFiltro ] = useState('')
+  const [ gastosFiltrados, setGastosFiltrados ] = useState([])
+
+
+
+  const handleNuevoPresupuesto = (presupuesto) => {
+    if(Number(presupuesto) > 0) {
+      setIsValidPresupuesto(true)
+    } else {
+      Alert.alert('Error', 'El Presupuesto no puede ser 0 o menor', 'Ok')
+
+    }
+  }
+ 
 
   return (
-    <View>
-    <Headers />
+    <View style={styles.contenedor}>
+   
+          <View style={styles.header}>
+              <Header />
+              
+              
+              {isValidPresupuesto ? (
+                <ControlPresupuesto 
+                    presupuesto={presupuesto}
+                    gastos={gastos}
+                    //resetearApp={resetearApp}
+                />
+              ) : (
+                  <NuevoPresupuesto 
+                      presupuesto={presupuesto}
+                      setPresupuesto={setPresupuesto}
+                      handleNuevoPresupuesto={handleNuevoPresupuesto}
+                  />
+              ) }
+             
+          </View>
+
+          {modal && (
+        <Modal
+            animationType='slide'
+            visible={modal}
+            onRequestClose={() => {
+              setModal(!modal)
+            }}
+        > 
+            <FormularioGasto 
+              setModal={setModal}
+              handleGasto={handleGasto}
+              gasto={gasto}
+              setGasto={setGasto}
+              eliminarGasto={eliminarGasto}
+            />
+        </Modal>
+      )}
+
+
+
+          {isValidPresupuesto && (
+        <Pressable
+          style={styles.pressable}
+          onPress={() => setModal(!modal)}
+        >
+          <Image
+              style={styles.imagen}
+              source={require('./src/img/nuevo-gasto.png')}
+          />
+        </Pressable>
+      ) }
+
+
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
+    contenedor: {
+      backgroundColor: '#F5F5F5',
+      flex: 1
+    },
+    header: {
+        backgroundColor: '#3B82F6',
+        minHeight: 400
+    },
+    pressable: {
+      width: 60,
+      height: 60, 
+      position: 'absolute',
+      bottom: 40,
+      right: 30
+    },  
+    imagen: {
+      width: 60,
+      height: 60
+    }
  
 });
 
